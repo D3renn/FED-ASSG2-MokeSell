59a2ed6b3f4004c4ef00c26785f69d04c5b5eedd
s10270502 <s10270502@connect.np.edu.sg>
2025-02-08 16:53:37 +0800

    Login page css update, Fixed kick back to login problem

--- Codes/src/scripts/utils.js
+++ Codes/src/scripts/utils.js
@@ -1,6 +1,6 @@
 export function checkUserLoggedIn (){
   let loginId = localStorage.getItem("loginId");
   if(loginId == undefined || loginId == ""){
-    window.location.href = "Login.html"; // This redirects to index.html
+    window.location.href = "/login/index.html"; // This redirects to index.html
   }
 }
